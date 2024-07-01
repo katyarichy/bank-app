7a692094c3d4015a62a75bcc6a9eb16c5043f710
Kateryna Marchenko <katya.richy@gmail.com>
2024-07-01 20:09:16 +0200

    add text for home and not found pages

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,5 +1,24 @@
+import { Box, Typography } from '@mui/material';
+
 const HomePage = () => {
-  return <div>home page</div>;
+  return (
+    <Box
+      sx={{
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        textAlign: 'center',
+        flexDirection: 'column',
+        minHeight: '100vh',
+        p: '2rem',
+      }}
+    >
+      <Typography variant='h4' component='h1' gutterBottom>
+        Welcome to the BANK APP
+      </Typography>
+      <Typography>Manage your bank accounts efficiently and easily.</Typography>
+    </Box>
+  );
 };
 
 export default HomePage;
--- src/pages/NotFoundPage.tsx
+++ src/pages/NotFoundPage.tsx
@@ -1,5 +1,21 @@
+import { Box, Typography } from '@mui/material';
+
 const NotFoundPage = () => {
-  return <div>not found page</div>;
+  return (
+    <Box
+      sx={{
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        textAlign: 'center',
+        flexDirection: 'column',
+        minHeight: '100vh',
+        p: '2rem',
+      }}
+    >
+      <Typography>Page not found!</Typography>
+    </Box>
+  );
 };
 
 export default NotFoundPage;
